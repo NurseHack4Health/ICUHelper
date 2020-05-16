3e45c3d3f4cf8721818310f714f13ec919711769
Isa Calderón <50374302+isacalderon@users.noreply.github.com>
2020-05-16 18:21:35 -0500

    No connection string

--- backend/ICUHelperFunctions/addInventory.cs
+++ backend/ICUHelperFunctions/addInventory.cs
@@ -65,7 +65,7 @@ namespace ICUHelperFunctions
 
         public static int WriteToDB(InventoryObject objInventory)
         {
-             string cnnString = "Server=nursehack.database.windows.net;Database=nursehackdb;Integrated Security=False;User ID=isacalderon;Password=SuperSecret!;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;"; 
+             string cnnString = Environment.GetEnvironmentVariable("DB_CONNECTION"); 
              int idInsert= 0; 
              StringBuilder sBquery = new StringBuilder();
              sBquery.Append("DECLARE @return_value int ");
