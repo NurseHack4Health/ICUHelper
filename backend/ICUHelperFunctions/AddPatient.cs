b3ad7c16abeb815c5d9f780856a1bbff76523690
Alejandra Angulo Rico <43595879+Alangulo@users.noreply.github.com>
2020-05-16 01:53:17 -0500

    insert Patient

--- backend/ICUHelperFunctions/AddPatient.cs
+++ backend/ICUHelperFunctions/AddPatient.cs
@@ -11,16 +11,20 @@ using System.Data.SqlClient;
 using System.Text.Json.Serialization;
 
 namespace ICUHelperFunctions
+
+
 {
     public static class AddPatient
     {
+
+
         [FunctionName("AddPatient")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             Patient auxObj = new Patient();
-           // auxObj.ObjUser = new User();
+            // auxObj.ObjUser = new User();
 
 
 
@@ -40,15 +44,19 @@ namespace ICUHelperFunctions
             auxObj.phone = req.Query["phone"];
             auxObj.emergencyContact = req.Query["emergencyContact"];
             auxObj.phoneEmergencyContact = req.Query["phoneEmergencyContact"];
-           // auxObj.dob = req.Query["dob"];
-            auxObj.idNumber = Int32.Parse( req.Query["idNumber"]);
+            // auxObj.dob = req.Query["dob"];
+            auxObj.idNumber = Int32.Parse(req.Query["idNumber"]);
             auxObj.idType = Int32.Parse(req.Query["idType"]);
+            auxObj.gender = Int32.Parse(req.Query["genderId"]);
 
             //auxObj.fullName = req.Query["fullName"];
-            auxObj.dob = DateTime.ParseExact(req.Query["fullName"], "yyyy-MM-dd",
-                                       System.Globalization.CultureInfo.InvariantCulture);
+
+            // auxObj.dob = DateTime.ParseExact(req.Query["dob"], "yyyy-MM-dd HH:mm:ss",
+            //   System.Globalization.CultureInfo.InvariantCulture);
+            DateTime fecha = Convert.ToDateTime(req.Query["dob"]);
+
             //auxObj.patientId= Int32.Parse(req.Query["patientId"]);
-          //  auxObj.phone = req.Query["phone"];
+            //  auxObj.phone = req.Query["phone"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
@@ -66,7 +74,7 @@ namespace ICUHelperFunctions
             {
 
 
-                int writeResult = WriteToDB(auxObj);
+                int writeResult = WriteToDB(auxObj,fecha);
 
                 if (writeResult >= 0)
                 {
@@ -86,35 +94,35 @@ namespace ICUHelperFunctions
 
 
 
-        public static int WriteToDB( Patient objPatient)
+        public static int WriteToDB(Patient objPatient, DateTime fecha)
         {
 
             string cnnString = Environment.GetEnvironmentVariable("DB_CONNECTION");
-
+            
             //  Console.WriteLine(cnnString);
-            int result=0;
-            using (SqlConnection connection = new SqlConnection(""))
-            {
-               // String query = "insert into [dbo].[patient] (user_id,condition_id) values (@userId,@conditionId)";
-                String query = "insert into [dbo].[users](full_name, phone)values(@full_name, @phone, @emergency_contact, @phone_emergency_contact, @gender_id, @date_of_birth, @identification_number, @identificaton_type); ";
-
+            int result = 0;
+            using (SqlConnection connection = new SqlConnection("")            {
+                // String query = "insert into [dbo].[patient] (user_id,condition_id) values (@userId,@conditionId)";
+                String query = "insert into [dbo].[users](full_name, phone,emergency_contact,phone_emergency_contact,gender_id,date_of_birth,identification_number,identificaton_type)values(@full_name, @phone, @emergency_contact, @phone_emergency_contact, @gender_id, @date_of_birth, @identification_number, @identificaton_type); ";
+                var sqlFormattedDate = objPatient.dob.Date.ToString("yyyy-MM-dd HH:mm:ss");
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     try
                     {
-
-                        command.Parameters.AddWithValue("@full_name", objPatient.userID);
+                        
+                        command.Parameters.AddWithValue("@full_name", objPatient.fullName);
                         command.Parameters.AddWithValue("@phone", objPatient.phone);
                         command.Parameters.AddWithValue("@emergency_contact", objPatient.emergencyContact);
                         command.Parameters.AddWithValue("@phone_emergency_contact", objPatient.phoneEmergencyContact);
                         command.Parameters.AddWithValue("@gender_id", objPatient.gender);
-                        command.Parameters.AddWithValue("@date_of_birth", objPatient.dob);
+                       // command.Parameters.AddWithValue("@date_of_birth", sqlFormattedDate);
+                        command.Parameters.AddWithValue("@date_of_birth", fecha);
                         command.Parameters.AddWithValue("@identification_number", objPatient.idNumber);
                         command.Parameters.AddWithValue("@identificaton_type", objPatient.idType);
-                        
+
 
                         connection.Open();
-                       result = command.ExecuteNonQuery();
+                        result = command.ExecuteNonQuery();
                     }
                     catch (Exception e)
                     {
@@ -122,14 +130,14 @@ namespace ICUHelperFunctions
                         Console.WriteLine("Error inserting data into Database!");
                         //return result;
                     }
-                    
 
-                       // return result;
-                    
 
-                    
-                  
-                   
+                    // return result;
+
+
+
+
+
 
                 }
 
